31889055625089d4c517337a3c75253ef152b077
HardBoss07 <matteo@bosshard.net>
2025-03-18 13:36:58 +0100

    updated NavBarComponent.tsx to include MyLittleZoo, changed formatting of NavBarComponent.tsx

--- MyLittleZoo/my-little-zoo/app/components/NavBarComponent.tsx
+++ MyLittleZoo/my-little-zoo/app/components/NavBarComponent.tsx
@@ -6,21 +6,24 @@ export default function NavBarComponment({ currentPage }: NavBarProps) {
     const navItems = ["Home", "Animals", "Contact", "About"];
 
     return (
-        <nav>
-            <ul className="list-none p-0 my-4 flex gap-6">
-                {navItems.map((item, index) => (
-                    <li key={index}>
-                        <a
-                            href={`${index === 0 ? "../" : item.toLowerCase()}`}
-                            className={`text-lg font-bold no-underline transition-colors duration-300 ease-in-out ${
-                                currentPage === index ? "text-[#FFD166]" : "text-white hover:text-[#FFD166]"
-                            }`}
-                        >
-                            {item}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <div className="bg-[#FF6B6B] p-7 pb-1.5 shadow-md">
+            <h1 className="text-4xl font-bold text-white">MyLittleZoo</h1>
+            <nav>
+                <ul className="list-none p-0 my-4 flex gap-6">
+                    {navItems.map((item, index) => (
+                        <li key={index}>
+                            <a
+                                href={`${index === 0 ? "../" : item.toLowerCase()}`}
+                                className={`text-lg font-bold no-underline transition-colors duration-300 ease-in-out ${
+                                    currentPage === index ? "text-[#FFD166]" : "text-white hover:text-[#FFD166]"
+                                }`}
+                            >
+                                {item}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
     );
 }
